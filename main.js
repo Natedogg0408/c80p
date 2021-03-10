5598daa66bbbaccc14fe7d72bb7811313eb400e1
Natedogg0408 <77352991+Natedogg0408@users.noreply.github.com>
2021-03-10 15:10:32 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function Make_A_Paragraph() {
+    var displaythep = [];
+
+        for(var i = 1; i<= 4; i++){}
+
+       displaythep.push(document.getElementById("para" + i).value);
+
+    
+   }
+   displaythep.join(". ")
+
+        document.getElementById("showhere").innerHTML=displaythep;
+
+console.log(Here is my code: fsf
+    SVGFEDropShadowElementf
+    sdf)
